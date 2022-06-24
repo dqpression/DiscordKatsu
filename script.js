2c3e7b4af3124243ea514f834bc85b3d7a546ae9
dqpression <62237025+dqpression@users.noreply.github.com>
2022-06-24 12:27:38 +0600

    Discord Server Katsunix

---
+++ script.js
@@ -0,0 +1,130 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if(this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
+    if(this.scrollY > 500) {
+      $('.scroll-up-btn').addClass("show");
+    } else {
+      $('.scroll-up-btn').removeClass("show");
+    }
+  })
+
+  $('.scroll-up-btn').click(function() {
+    $('html').animate({scrollTop: 0});
+  })
+  
+  $('.menu-btn').click(function() {
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+  })
+
+  var typed = new Typed('.typing', {
+    strings: [
+      "являемся серверными ботами",
+      "мощные",
+      "просты в использовании",
+      "милые",
+      "любим тебя",
+      "сделаны с любовью",
+      "горячие",
+      "имеем множество крутых команд",
+      "хороши",
+    ],
+    typeSpeed: 120,
+    backSpeed: 80,
+    loop: true,
+  })
+
+  // this is really cool
+  // but you should vote Mikuni first
+
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0:{
+        items: 1,
+        nav: false
+      },
+      600:{
+        items: 2,
+        nav: false
+      },
+      1000:{
+        items: 3,
+        nav: false
+      }
+    }
+  });
+
+})
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if(this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
+    if(this.scrollY > 500) {
+      $('.scroll-up-btn').addClass("show");
+    } else {
+      $('.scroll-up-btn').removeClass("show");
+    }
+  })
+
+  $('.scroll-up-btn').click(function() {
+    $('html').animate({scrollTop: 0});
+  })
+  
+  $('.menu-btn').click(function() {
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+  })
+
+  var typed = new Typed('.typing1', {
+    strings: [
+      "Katsu",
+      "Katsu Music",
+      "Moderation",
+      "VK Music",
+      "Vexera",
+      "Juniper Bot",
+      "Chip",
+      "Carl-bot",
+      "UnbelievaBoat"
+    ],
+    typeSpeed: 120,
+    backSpeed: 80,
+    loop: true,
+  })
+
+  // this is really cool
+  // but you should vote Mikuni first
+
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0:{
+        items: 1,
+        nav: false
+      },
+      600:{
+        items: 2,
+        nav: false
+      },
+      1000:{
+        items: 3,
+        nav: false
+      }
+    }
+  });
+
+})
